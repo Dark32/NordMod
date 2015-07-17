f32b1eb630abccb6225cb08a292b9bff35798a94
Андрей Сурков <a.surkov@i2013.ru>
2015-07-17 22:34:18 +0300

    Добавлены Текстуры, самородки, кристаллы
    Добавлено перечесление самородков и связь с рудами
    Добавлено перечесление кристаллов и связь с рудами
    Добавлены|обновлены текстуры блоков и слитков металла
    Добавлены текстуры чистых блоков металла
    Добавлены текстуры блоков кристаллов
    Убраны неиспользуемые текстур блоков метала другого типа
    Добавлены текстуры чистых слитков
    Добавлены текстуры чистых измельчённых осколков руды
    Добавлены текстуры кристаллов
    Добавлены текстуры грязных измельчённых осколков руды
    Добавлены текстуры осколков руды
    Добавлены текстуры измельчённых слитков, не все
    Добавлены текстыры самородков, кроме ртути.

--- src/main/java/ru/nord/common/lib/utils/enums/EnumOre.java
+++ src/main/java/ru/nord/common/lib/utils/enums/EnumOre.java
@@ -3,21 +3,56 @@ package ru.nord.common.lib.utils.enums;
 import net.minecraft.util.IStringSerializable;
 
 public enum EnumOre implements IStringSerializable {
-    SILVER      (0,  "silver",      "silver"),
-    GALENA      (1,  "galena",      "silver"),
-    TIN         (2,  "tin",         "tin"),
-    ZINCITE     (3,  "zincite",     "zinc"),
-    BAUXITE     (4,  "bauxite",     "aluminum"),
-    UVAROVITE   (5,  "uvarovite",   "chrom"),
-    CROCOITE    (6,  "crocoite",    "chrom"),
-    COPPER      (7,  "copper",      "copper"),
-    OLIVINE     (9,  "olivine",     "iron"),
-    MAGNETITE   (10, "magnetite",   "iron"),
-    LIMONITE    (11, "limonite",    "iron"),
-    ALMANDINE   (12, "almandine",   "iron"),
-    PENTLANDITE (13, "pentlandite", "nicel"),
-    MILLERITE   (14, "millerite",   "nicel"),
-    MERCURY     (15, "mercury",     "mercury");
+    SILVER(0, "silver", "silver") {
+        @Override
+        public EnumNugget getNugget() {
+            return EnumNugget.SILVER;
+        }
+    },
+    GALENA(1, "galena", "silver"),
+    TIN(2, "tin", "tin") {
+        @Override
+        public EnumNugget getNugget() {
+            return EnumNugget.TIN;
+        }
+    },
+    ZINCITE(3, "zincite", "zinc"),
+    BAUXITE(4, "bauxite", "aluminum"),
+    UVAROVITE(5, "uvarovite", "chrom") {
+        @Override
+        public EnumCrystal getCrystal() {
+            return EnumCrystal.UVAROVITE;
+        }
+    },
+    CROCOITE(6, "crocoite", "chrom") {
+        @Override
+        public EnumCrystal getCrystal() {
+            return EnumCrystal.CROCOITE;
+        }
+    },
+    COPPER(7, "copper", "copper"),
+    OLIVINE(9, "olivine", "iron") {
+        @Override
+        public EnumCrystal getCrystal() {
+            return EnumCrystal.OLIVINE;
+        }
+    },
+    MAGNETITE(10, "magnetite", "iron"),
+    LIMONITE(11, "limonite", "iron"),
+    ALMANDINE(12, "almandine", "iron") {
+        @Override
+        public EnumCrystal getCrystal() {
+            return EnumCrystal.ALMANDINE;
+        }
+    },
+    PENTLANDITE(13, "pentlandite", "nicel"),
+    MILLERITE(14, "millerite", "nicel"),
+    MERCURY(15, "mercury", "mercury") {
+        @Override
+        public EnumNugget getNugget() {
+            return EnumNugget.MERCURY;
+        }
+    },;
 
 
     private final String name;
@@ -55,4 +90,11 @@ public enum EnumOre implements IStringSerializable {
         return this.meta;
     }
 
+    public EnumNugget getNugget() {
+        return null;
+    }
+
+    public EnumCrystal getCrystal() {
+        return null;
+    }
 }
