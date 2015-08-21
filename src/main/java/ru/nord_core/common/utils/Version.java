572158ef13a20d28359c65116644f64874a3d6da
Андрей Сурков <a.surkov@i2013.ru>
2015-08-21 21:37:49 +0300

    Обновление версии

--- src/main/java/ru/nord_core/common/utils/Version.java
+++ src/main/java/ru/nord_core/common/utils/Version.java
@@ -4,8 +4,8 @@ import net.minecraftforge.fml.common.ModMetadata;
 
 public class Version {
     public static final String MODID = "nord_core";
-    public static final String VERSION = "0.0.1";
-    public static final String NAME = "Nord Mod 1.8";
+    public static final String VERSION = "0.1.0";
+    public static final String NAME = "Core Nord Mod 1.8";
     public static void setupModInfo(ModMetadata meta){
         meta.authorList.add("dark32");
         meta.authorList.add("BluDiss");
