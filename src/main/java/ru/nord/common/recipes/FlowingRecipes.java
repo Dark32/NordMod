68f7fe08fe56aa7033d1acebb5aafe7bb6e4349d
Андрей Сурков <a.surkov@i2013.ru>
2015-08-27 21:47:10 +0300

    Рецепты
    Плавильня и Дробитель

--- src/main/java/ru/nord/common/recipes/FlowingRecipes.java
+++ src/main/java/ru/nord/common/recipes/FlowingRecipes.java
@@ -1,43 +1,108 @@
 package ru.nord.common.recipes;
 
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.fml.common.FMLLog;
+import ru.nord.NordBloks;
 import ru.nord.NordItems;
 import ru.nord.common.lib.recipes.FlowingRecipes1I2O;
-import ru.nord_core.common.utils.enums.EnumMetal;
+import ru.nord_core.common.utils.enums.*;
 
-/**
- * Created by andrew on 03.08.15.
- */
 public class FlowingRecipes {
-    public static void postInit() {
-        FlowingRecipes1I2O.addRecipe(new ItemStack(Item.getItemFromBlock(Blocks.gold_ore), 1),
-                new ItemStack(NordItems.itemOreDrop, 2, 1),
-                new ItemStack(Item.getItemFromBlock(Blocks.cobblestone), 1)
-                , 80, 90, 0);
-        FlowingRecipes1I2O.addRecipe(new ItemStack(Item.getItemFromBlock(Blocks.iron_ore), 1),
-                new ItemStack(NordItems.itemOreDrop, 2, 0),
-                new ItemStack(Item.getItemFromBlock(Blocks.cobblestone), 1)
-                , 80, 90, 0);
-/*
-        for (EnumOre ore : EnumOre.values()) {
+    private static final int oreToDrop = 8;
+    private static final int oreToNugget = 11;
 
-            FlowingRecipes1I2O.addRecipe(
-                    new ItemStack(NordBloks.metalOre,1,ore.getMetadata()),
-                    new ItemStack(NordItems.itemOreDrop,2,ore.getDust().getMetadata()),
-                    new ItemStack(Item.getItemFromBlock(Blocks.cobblestone),1)
-                    ,80,90,0);
+    public static void postInit() {
+
+        FlowingRecipes1I2O.addRecipe(new ItemStack(Item.getItemFromBlock(Blocks.stone), 1),
+                new ItemStack(Item.getItemFromBlock(Blocks.cobblestone)),
+                null, 80, 0, 0);
+        FlowingRecipes1I2O.addRecipe(new ItemStack(Item.getItemFromBlock(Blocks.cobblestone), 1),
+                new ItemStack(Item.getItemFromBlock(Blocks.gravel)),
+                null , 80, 0, 0);
+        FlowingRecipes1I2O.addRecipe(new ItemStack(Item.getItemFromBlock(Blocks.gravel), 1),
+                new ItemStack(Item.getItemFromBlock(Blocks.sand)),
+                new ItemStack(Items.flint, 1)
+                , 80, 10, 0);
+        FlowingRecipes1I2O.addRecipe(new ItemStack(Item.getItemFromBlock(Blocks.redstone_ore), 1),
+                new ItemStack(Items.redstone, 12),
+                new ItemStack(NordItems.itemOreNugget, 1, EnumNugget.MERCURY.getMetadata())
+                , 80, 10, 0);
+
+        /*
+        Измалываем руду в 6 дропов
+         */
+        FlowingRecipes1I2O.addRecipe(new ItemStack(Item.getItemFromBlock(Blocks.iron_ore), 1),
+                new ItemStack(NordItems.itemOreDrop, oreToDrop, EnumOreDrop.MAGNETITE.getMetadata()),
+                new ItemStack(Item.getItemFromBlock(Blocks.cobblestone), 1)
+                , 80, 10, 0);
+
+        FlowingRecipes1I2O.addRecipe(new ItemStack(Item.getItemFromBlock(Blocks.gold_ore), 1),
+                new ItemStack(Items.gold_nugget, oreToNugget),
+                new ItemStack(Item.getItemFromBlock(Blocks.cobblestone), 1)
+                , 80, 90, 0);
+        for (EnumOre ore : EnumOre.values()) {
+            if (ore.getOreDrop() != null)
+                FlowingRecipes1I2O.addRecipe(
+                        new ItemStack(NordBloks.metalOre, 1, ore.getMetadata()),
+                        new ItemStack(NordItems.itemOreDrop, oreToDrop, ore.getOreDrop().getMetadata()),
+                        new ItemStack(Item.getItemFromBlock(Blocks.sand), 1)
+                        , 80, 80, 0);
+            if (ore.getNugget() != null) {
+                FlowingRecipes1I2O.addRecipe(
+                        new ItemStack(NordBloks.metalOre, 1, ore.getMetadata()),
+                        new ItemStack(NordItems.itemOreNugget, oreToNugget, ore.getNugget().getMetadata()),
+                        new ItemStack(Item.getItemFromBlock(Blocks.sand), 1)
+                        , 80, 80, 0);
+            }
         }
-        */
+ /*
+        Измалываем дропы  в грязную пыль
+         */
+        for (EnumOreDrop ore : EnumOreDrop.values()) {
+            if (ore.getDust() != null)
+                FlowingRecipes1I2O.addRecipe(
+                        new ItemStack(NordItems.itemOreDrop, 1, ore.getMetadata()),
+                        new ItemStack(NordItems.itemOreDirtyPowder, 1, ore.getDust().getMetadata()),
+                        new ItemStack(NordItems.itemOreDirtyPowder, 1, ore.getDust().getMetadata()),
+                        80, 5, 0);
+        }
+
+        /*
+        Измалывыаем слитки в пыль
+         */
+        FlowingRecipes1I2O.addRecipe(new ItemStack(Items.iron_ingot, 1),
+                new ItemStack(NordItems.itemMetalDust, 1, EnumDust.IRON.getMetadata()),
+                null, 80, 0, 0);
+
+        FlowingRecipes1I2O.addRecipe(new ItemStack(Items.gold_ingot, 1),
+                new ItemStack(NordItems.itemMetalDust, 1, EnumDust.GOLD.getMetadata()),
+                null, 80, 0, 0);
+
         for (EnumMetal metal : EnumMetal.values()) {
             FlowingRecipes1I2O.addRecipe(
                     new ItemStack(NordItems.itemIngot, 1, metal.getMetadata()),
-                    new ItemStack(NordItems.itemOreDrop, 1, metal.getDust().getMetadata()),
-                    new ItemStack(Item.getItemFromBlock(Blocks.cobblestone), 1)
-                    , 80, 0, 0);
+                    new ItemStack(NordItems.itemMetalDust, 1, metal.getDust().getMetadata()),
+                    null, 80, 0, 0);
         }
-
+        for (EnumClearMetal metal : EnumClearMetal.values()) {
+            if (metal == EnumClearMetal.IRON) {
+                FlowingRecipes1I2O.addRecipe(
+                        new ItemStack(NordItems.itemClearIngot, 1, metal.getMetadata()),
+                        new ItemStack(NordItems.itemMetalDust, 1, EnumDust.IRON.getMetadata()),
+                        null, 80, 0, 0);
+            } else if (metal == EnumClearMetal.GOLD) {
+                FlowingRecipes1I2O.addRecipe(
+                        new ItemStack(NordItems.itemClearIngot, 1, metal.getMetadata()),
+                        new ItemStack(NordItems.itemMetalDust, 1, EnumDust.GOLD.getMetadata()),
+                        null, 80, 0, 0);
+            } else {
+                FlowingRecipes1I2O.addRecipe(
+                        new ItemStack(NordItems.itemClearIngot, 1, metal.getMetadata()),
+                        new ItemStack(NordItems.itemMetalDust, 1, metal.getMetal().getDust().getMetadata()),
+                        null, 80, 0, 0);
+            }
+        }
     }
 }
--- src/main/java/ru/nord/common/recipes/FurnaceRecipes.java
+++ src/main/java/ru/nord/common/recipes/FurnaceRecipes.java
@@ -1,10 +1,11 @@
 package ru.nord.common.recipes;
 
-import net.minecraft.init.Blocks;
-import net.minecraft.item.Item;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import ru.nord.NordItems;
 import ru.nord.common.lib.recipes.FurnaceRecipes1I2O;
+import ru.nord_core.common.utils.enums.EnumDust;
+import ru.nord_core.common.utils.enums.EnumOreDrop;
 
 import java.util.Iterator;
 import java.util.Map;
@@ -12,23 +13,67 @@ import java.util.Map;
 public class FurnaceRecipes {
     public static void postInit() {
         addVanilaRecipes();
-        FurnaceRecipes1I2O.addRecipe(new ItemStack(Item.getItemFromBlock(Blocks.gold_block), 1),
-                new ItemStack(NordItems.itemOreDrop, 2, 1),
-                new ItemStack(Item.getItemFromBlock(Blocks.cobblestone), 1)
-                , 80, 90, 0);
+        /*
+        Плавим дпропы
+         */
+        for (EnumOreDrop ore : EnumOreDrop.values()) {
+            System.err.println(ore + " " + ore.getMetadata() + " " + ore.getDust().getMetal());
+            if (ore.getDust() == EnumDust.IRON) {
+                FurnaceRecipes1I2O.addRecipe(
+                        new ItemStack(NordItems.itemOreClearPowder, 1, ore.getMetadata()),
+                        new ItemStack(Items.iron_ingot, 1), null, 200, 0, 0);
+                FurnaceRecipes1I2O.addRecipe(
+                        new ItemStack(NordItems.itemOreDirtyPowder, 3, ore.getMetadata()),
+                        new ItemStack(Items.iron_ingot, 2), null, 200, 0, 0);
+
+            } else if (ore.getDust() == EnumDust.GOLD) {
+                FurnaceRecipes1I2O.addRecipe(
+                        new ItemStack(NordItems.itemOreClearPowder, 1, ore.getMetadata()),
+                        new ItemStack(Items.gold_ingot, 1), null, 200, 0, 0);
+                FurnaceRecipes1I2O.addRecipe(
+                        new ItemStack(NordItems.itemOreDirtyPowder, 3, ore.getMetadata()),
+                        new ItemStack(Items.gold_ingot, 2), null, 200, 0, 0);
+            } else if (ore.getDust().getMetal() != null) {
+                FurnaceRecipes1I2O.addRecipe(
+                        new ItemStack(NordItems.itemOreClearPowder, 1, ore.getMetadata()),
+                        new ItemStack(NordItems.itemIngot, 1, ore.getDust().getMetal().getMetadata()),
+                        null, 200, 0, 0);
+                FurnaceRecipes1I2O.addRecipe(
+                        new ItemStack(NordItems.itemOreDirtyPowder, 3, ore.getMetadata()),
+                        new ItemStack(NordItems.itemIngot, 2, ore.getDust().getMetal().getMetadata()),
+                        null, 200, 0, 0);
+            }
+        }
+        for (EnumOreDrop ore2 : EnumOreDrop.values()) {
+            if (ore2.getDust() == EnumDust.IRON) {
+                ItemStack is = new ItemStack(NordItems.itemOreDrop, 2, ore2.getMetadata());
+                System.err.println(is +" "+ ore2+ " "+ ore2.getDust());
+                FurnaceRecipes1I2O.addRecipe(
+                        is,
+                        new ItemStack(Items.iron_ingot, 1), null, 200, 0, 0);
+            } else if (ore2.getDust() == EnumDust.GOLD) {
+                FurnaceRecipes1I2O.addRecipe(
+                        new ItemStack(NordItems.itemOreDrop, 2, ore2.getMetadata()),
+                        new ItemStack(Items.gold_ingot, 1), null, 200, 0, 0);
+            } else if (ore2.getDust().getMetal() != null) {
+                FurnaceRecipes1I2O.addRecipe(
+                        new ItemStack(NordItems.itemOreDrop, 2, ore2.getMetadata()),
+                        new ItemStack(NordItems.itemIngot, 1, ore2.getDust().getMetal().getMetadata()),
+                        null, 200, 0, 0);
+            }
+        }
     }
 
-    private static void addVanilaRecipes(){
+    private static void addVanilaRecipes() {
         Map recipes = net.minecraft.item.crafting.FurnaceRecipes.instance().getSmeltingList();
         Iterator iterator = recipes.entrySet().iterator();
         Map.Entry entry;
 
-        while (iterator.hasNext())
-        {
-            entry = (Map.Entry)iterator.next();
-            ItemStack input = (ItemStack)entry.getKey();
-            ItemStack output = (ItemStack)entry.getValue();
-            FurnaceRecipes1I2O.addRecipe(input,output,output,200,0,0);
+        while (iterator.hasNext()) {
+            entry = (Map.Entry) iterator.next();
+            ItemStack input = (ItemStack) entry.getKey();
+            ItemStack output = (ItemStack) entry.getValue();
+            FurnaceRecipes1I2O.addRecipe(input, output, output, 200, 0, 0);
         }
 
     }
